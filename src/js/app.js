856d7fd8a8f28f6cb4ba4f568b46f506eac81ab2
Matt Hall <bizear@gmail.com>
2012-05-19 15:51:03 +0100

    Added in functionality to delete a node.

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ var log = function(message) {
 		window.console.log(message);
 	}
 };
-	
+
 // Node model.
 var Node = Backbone.Model.extend({
 	defaults: {
@@ -12,22 +12,22 @@ var Node = Backbone.Model.extend({
 		em: 1,
 		context: 16
 	},
-			
+	
 	initialize: function() {
 		this.bind("change:px", function() {
 			var px = this.get("px");
-				
+			
 			// Re-calc the ems once the pixels have been changed.
 			this.calcEms(px, 16); // Hardcoded the context at "16" for now.
 		});
 	},
-			
+	
 	// validate: function(attributes) {
 	// 	if (/\D/g.test(attributes.px)) {
 	// 		return "Please enter a number.";
 	// 	}
 	// },
-			
+	
 	calcEms: function(target, context) {
 		if ($.isNumeric(target)) { // Don't bother if target's not a number.
 			var result = target / context;
@@ -39,24 +39,22 @@ var Node = Backbone.Model.extend({
 		}
 	}
 });
-	
+
 // Nodes collection.
 var Nodes = Backbone.Collection.extend({
-	model: new Node(),
-		
 	initialize: function() {
 		this.add(new Node());
 	}
 });
-	
+
 // Node view.
 var NodeView = Backbone.View.extend({
 	tagName: "li",
-		
+	
 	className: "node",
-		
+	
 	tmpl: _.template($("#node-template").html()),
-		
+	
 	events: {
 		"dblclick .name": "toggleNodeName",
 		"blur input.node-name": "updateNodeName",
@@ -65,22 +63,22 @@ var NodeView = Backbone.View.extend({
 		"click .js-add-sub-node": "addSub",
 		"click .delete": "delete"
 	},
-		
+	
 	initialize: function() {
 		this.render();
 	},
-		
+	
 	render: function() {
 		this.$el.html(this.tmpl(this.model.attributes));
-			
+	
 		return this;
 	},
-		
+	
 	toggleNodeName: function() {
 		var $el = this.$el,
 			$name = $el.find(".name"),
 			$nodeName = $el.find("input.node-name");
-				
+		
 		if ($name.is(":visible")) {
 			$name.hide();
 			$nodeName.show().select();
@@ -90,86 +88,90 @@ var NodeView = Backbone.View.extend({
 			$nodeName.hide();
 		}
 	},
-			
+	
 	updateNodeName: function() {
 		var val = this.$el.find("input.node-name").val(),
 			$name = this.$el.find(".name");
-				
+		
 		this.model.set("name", val);
 		$name.text(val);
-				
+		
 		this.toggleNodeName();
 	},
-		
+	
 	updatePx: function() {
 		var $pxField = this.$el.find("input.px"),
 			val = parseInt($pxField.val(), 10);
-				
+		
 		// Update px field with the parsed integer
 		// to remove any non-numeric characters.
 		$pxField.val(val);
-				
+		
 		this.model.set("px", val);
-				
+		
 		this.updateEm();
 	},
-		
+	
 	updateEm: function() {
 		this.$el.find("input.ems").val(this.model.get("em") + "em");
 	},
-			
+	
 	addNew: function() {
 		var idx = nodesView.collection.indexOf(this.model);
-			
+		
 		nodesView.collection.add(new Node(), {
 			at: idx + 1
 		});
-			
-		log(idx);
 	},
-			
+	
 	addSub: function() {
 		this.subNodeList = new NodesView({
 			context: this.$el
 		});
-			
+		
 		log(this.subNodeList.context);
 	},
-			
+	
 	delete: function() {
-		log("Delete");
+		var collection = nodesView.collection;
+		
+		if (collection.length > 1) { // Don't remove if only one node remains.
+			collection.remove(this.model);
+		}
 	}
 });
 	
 // Node collection.
 var NodesView = Backbone.View.extend({
 	tagName: "ul",
-		
+	
 	className: "node-list",
-		
+	
 	context: "#em-calc",
-		
+	
 	initialize: function() {
 		_.bindAll(this);
+		
 		this.collection = new Nodes();
 		this.collection.bind("add", this.render);
+		this.collection.bind("remove", this.render);
 		
 		this.render();
 	},
-		
+	
 	render: function() {
 		var that = this;
-			
+		
 		that.$el.html("");
-			
+		
 		that.collection.each(function(node) {
 			that.$el.append(new NodeView({
 				model: node
 			}).render().el);
 		});
-			
+		
 		that.$el.appendTo(that.context);
-			
+		
 		return that;
 	}
 });
@@ -177,7 +179,7 @@ var NodesView = Backbone.View.extend({
 // App.
 var AppView = Backbone.View.extend({
 		el: $("#em-calc"),
-			
+		
 		initialize: function() {
 			this.$el.on("click", "input.ems", function(e) {
 				this.select();
